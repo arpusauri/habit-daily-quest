ab4f3bd8d1d481a9082dc1aa792b0cf86697d174
AryaPS <110978840+arpusauri@users.noreply.github.com>
2026-08-31 17:55:07 +0700

    fix analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import UserProfile from "./components/UserProfileSection";
 import QuestSection from "./components/QuestSection";
